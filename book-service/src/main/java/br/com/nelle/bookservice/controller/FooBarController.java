7f3da7a2218e1d089fc065472950e9c22285e1b5
Bruno Ramon <brmachado_5@hotmail.com>
2021-10-13 16:32:15 -0300

    utilizando Circuit braker para melhor gerenciar as falhas nas requisicoes das APIs

--- book-service/src/main/java/br/com/nelle/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/br/com/nelle/bookservice/controller/FooBarController.java
@@ -1,5 +1,6 @@
 package br.com.nelle.bookservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +16,17 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar")
+//    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
     public String fooBar(){
         logger.info("Request to foo-bar is received");
 
         var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
 //        return "Foo-Bar!!!";
         return response.getBody();
+    }
+
+    public String fallbackMethod(Exception ex){
+        return "fallbackMethod foo-bar!!!";
     }
 }
